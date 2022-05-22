a49eab769bd26e0bc19b425050135179b2b8e4d7
elliott dombrowski <elliottroyal@me.com>
2022-05-22 01:33:22 -0500

    set up switcher for cms

--- client/src/pages/CMS/CMS.js
+++ client/src/pages/CMS/CMS.js
@@ -8,22 +8,34 @@ import './styles.scss';
 import './query.scss';
 
 const CMS = () => {
+  const [switchCMS, setSwitchCMS] = useState(false);
+
   return (
     <main className='cms-wrapper'>
       <nav>
         <ul className='cms-nav'>
           <li>
-            <Link to='/'>
+            <Link 
+              to='/'
+              onClick={() => setSwitchCMS(prev => !prev)}
+              >
               Edit Events
             </Link>
           </li>
           <li>
-            <Link to='/'>
+            <Link 
+              to='/'
+              onClick={() => setSwitchCMS(prev => !prev)}
+            >
               Edit Products
             </Link>
           </li>
         </ul>
       </nav>
+
+      <section className='cms-selection'>
+        {switchCMS ? <StoreCMS /> : <EventsCMS />}
+      </section>
     </main>
   );
 };
